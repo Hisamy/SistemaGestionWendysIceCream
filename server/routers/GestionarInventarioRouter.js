5c23ab4e4243a816ad4eb7a65e1a7824d62e5fb9
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-03-25 21:12:54 -0700

    router ubicado por el servidor

---
+++ server/routers/GestionarInventarioRouter.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const gestionarInventarioRouter = Router();
+
+export default gestionarInventarioRouter;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import gestionarInventarioRouter from './routers/GestionarInventarioRouter.js';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/gestionarInventario', gestionarInventarioRouter);
+
 
 app.listen(port, () => {
   console.log(`Servidor activo en puerto ${port}`);
