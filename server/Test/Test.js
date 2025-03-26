867ebaf9d836236515ab23115ab1c8f2d39fd65e
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-03-26 03:25:02 -0700

    Todo el negocios/servicio TERMINADO
    lo dificil realmente fue la bd... sqlite me caes mal😣

--- server/Test/Test.js
+++ server/Test/Test.js
@@ -1,31 +1,66 @@
 import GestionarInventarioRepository from '../repositories/GestionarInventarioRepository.js';
 import Consumible from '../entities/Consumible.js';
+import GestionarInventarioService from '../services/GestionarInventarioService.js';
 import { connection } from '../database/Connection.js';
 
 async function init() {
     try {
 
-        //const consumiblePrueba = new Consumible('Cucharas', 5);
-        const repo = new GestionarInventarioRepository();
+        const serv = new GestionarInventarioService();
+        /**
+            const consumiblePrueba = new Consumible('Roblox', 5);
+    
+            await serv.registrarConsumible(consumiblePrueba);
+    
+            console.log("El servicio registró correctamente el consumible");
+            console.log(consumiblePrueba);
+        */
+        
+
+        const obtenidos = await serv.obtenerConsumibles();
+        console.log("Estos son los consumibles obtenidos del servicio");
+        console.log(obtenidos.map(consumible => consumible.nombre));
+
+        /**
+            const obtenido = await serv.obtenerConsumible(2);
+            console.log("Este consumible se obtuvo del servicio", obtenido.nombre);
+    
+            await serv.editarConsumible(3,{nombre:"Papel", cantidad:32});
+            console.log("Se editó el consumible correctamente");
+    
+            const obtenidosNuevos = await serv.obtenerConsumibles();
+            console.log("Estos son los consumibles obtenidos del servicio");
+            console.log(obtenidosNuevos.map(consumible => consumible.nombre));
+        */
+
+        await serv.eliminarConsumible(3);
+        const obtenidosFinal = await serv.obtenerConsumibles();
+        console.log("Estos son los consumibles obtenidos del servicio");
+        console.log(obtenidosFinal.map(consumible => consumible.nombre));
+
+
+        //const repo = new GestionarInventarioRepository();
         //await repo.guardarConsumible(consumiblePrueba);
     
         //console.log("Objeto creado en prueba:");
         //console.log(consumiblePrueba.nombre);
         
 
-        const consumibleObtenidos = await repo.obtenerTodosLosConsumibles();
-        
-        console.log("Consumibles obtenidos de la base de sqlito");    
-        console.log(consumibleObtenidos.map(consumible => consumible.nombre));
-
-        const consumibleEspecifico = await repo.obtenerConsumiblePorId(1);
-        console.log("Consumible obtenido por id: ", consumibleEspecifico.nombre);
-
-        //const consumibleEditado = await repo.editarConsumible(1, {nombre: "Pañales", cantidad: 10});
-        //console.log("Consumible editado: ", consumibleEditado.nombre);
-
-        const consumibleEliminado = await repo.eliminarConsumible(1);
-        console.log("Consumible eliminado: ", consumibleEliminado.nombre);
+        /**
+            const consumibleObtenidos = await repo.obtenerTodosLosConsumibles();
+            
+            console.log("Consumibles obtenidos de la base de sqlito");    
+            console.log(consumibleObtenidos.map(consumible => consumible.nombre));
+    
+            const consumibleEspecifico = await repo.obtenerConsumiblePorId(1);
+            console.log("Consumible obtenido por id: ", consumibleEspecifico.nombre);
+    
+            //const consumibleEditado = await repo.editarConsumible(1, {nombre: "Pañales", cantidad: 10});
+            //console.log("Consumible editado: ", consumibleEditado.nombre);
+    
+            const consumibleEliminado = await repo.eliminarConsumible(1);
+            console.log("Consumible eliminado: ", consumibleEliminado.nombre);
+        */
 
 
     } catch (error) {
--- server/services/GestionarInventarioService.js
+++ server/services/GestionarInventarioService.js
@@ -1,11 +1,56 @@
+import GestionarInventarioRepository from "../repositories/GestionarInventarioRepository.js";
+
 class GestionarInventarioService {
+
+    constructor(){
+        this.repository = new GestionarInventarioRepository();
+    }
 
     async registrarConsumible(consumible){
         try {
-            
+            const registroConsumible = await this.repository.guardarConsumible(consumible);
+            return registroConsumible;
         } catch (error) {
-            
+            console.error("Error del servicio al registrar los datos", error);
+        }
+    }
+
+    async obtenerConsumibles(){
+        try {
+            const consumiblesObtenidos = await this.repository.obtenerTodosLosConsumibles();
+            return consumiblesObtenidos;
+        } catch (error) {
+            console.error("Error del servicio al obtener los datos", error);
+        }
+    }
+
+    async obtenerConsumible(id){
+        try {
+            const consumibleObtenido = await this.repository.obtenerConsumiblePorId(id);
+            return consumibleObtenido;
+        } catch (error) {
+            console.error("Error del servicio al obtener los datos", error);
+        }
+    }
+
+    async editarConsumible(id, consumible){
+        try {
+            const consumibleObtenido = await this.repository.editarConsumible(id,consumible);
+            return consumibleObtenido;
+        } catch (error) {
+            console.error("Error del servicio al modificar datos", error);
+        }
+    }
+
+    async eliminarConsumible(id){
+        try {
+            const consumibleEliminado = await this.repository.eliminarConsumible(id);
+            return consumibleEliminado;
+        } catch (error) {
+            console.error("Error del servicio al modificar datos", error);
         }
     }
 
 }
+
+export default GestionarInventarioService;
