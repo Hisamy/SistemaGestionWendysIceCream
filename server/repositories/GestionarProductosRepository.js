5c8f30734758fdca65b8e44bd0bf0274f6e16372
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2025-03-30 15:54:35 -0700

    Create GestionarProductosRepository.js

---
+++ server/repositories/GestionarProductosRepository.js
@@ -0,0 +1,46 @@
+import { ILike, Not } from 'typeorm';
+import { connection } from '../database/Connection.js';
+import { ProductoSchema } from '../entities/Producto.js';
+import { DatabaseError} from '../errors/DatabaseError.js';
+
+class GestionarInventarioRepository {
+
+    constructor() {
+        this.repo = connection.getRepository(ProductoSchema);
+    }
+
+    async guardarProducto(producto) {
+        try {
+            return await this.repo.save(producto)
+        } catch (error) {
+            throw new DatabaseError(`Error al guardar consumible: ${error.message}`, error);
+        }
+    }
+
+    async obtenerProductoPorId(id) {
+        try {
+            const producto = await this.repo.findOneBy({ id });
+            return producto;
+        } catch (error) {
+            throw new DatabaseError(`Error al obtener consumible con ID ${id}: ${error.message}`, error);
+        }
+    }
+
+    async obtenerTodosLosProductos() {
+        try {
+            const productos = await this.repo.find() || [];
+            return productos;
+        } catch (error) {
+            throw new DatabaseError(`Error al obtener todos los productos: ${error.message}`, error);
+        }
+    }
+
+    async obtenerProductosPorNombre(nombre) {
+        try {
+            const productos = await this.repo.find({ where: {nombre: ILike(`%${nombre}%`) } }) || [];
+            return productos;
+        } catch (error) {
+            throw new DatabaseError(`Error al obtener productos con nombre similar a "${nombre}": ${error.message}`, error);
+        }
+    }
+}
