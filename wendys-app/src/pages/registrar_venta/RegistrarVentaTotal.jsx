cc404e5940f4f04bdd074247f10f53405aa8c885
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-04-29 20:50:27 -0700

    errorsito de dedo

--- wendys-app/src/pages/registrar_venta/RegistrarVentaTotal.jsx
+++ wendys-app/src/pages/registrar_venta/RegistrarVentaTotal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';s
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import NavLeft from '../../components/nav_left/NavLeft.jsx';
 import PinkRectangle from '../../components/main_content/PinkRectangle.jsx';
