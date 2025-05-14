a0d928e94db24fac63cb170694897421685ae479
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-05-14 01:17:09 -0700

    Diseño de inicio terminado

--- wendys-app/src/pages/gestionar_producto/registrar-producto/editar-producto/EditarProducto.jsx
+++ wendys-app/src/pages/gestionar_producto/registrar-producto/editar-producto/EditarProducto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import NavLeft from '../../components/nav_left/NavLeft.jsx';
+import NavLeft from "../../../../components/nav_left/NavLeft";
 
 
 function EditarProducto() {
